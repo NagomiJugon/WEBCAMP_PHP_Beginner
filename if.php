fd2406d74a95c1bf6a9316f8133f856d5f823377
EC2 Default User <ec2-user@ip-172-31-77-95.ec2.internal>
2022-03-14 14:28:53 +0000

    if.phpを書いた

---
+++ if.php
@@ -0,0 +1,38 @@
+<?php
+
+//
+if ( true ) {
+    echo "true　だとここを通る？？？ \n";
+}
+
+//
+if ( false ) {
+    echo "false　だとここを通る？？？ \n";
+}
+
+//
+$age = 19 ;
+if ( $age >= 20 ) {
+    echo "成人です \n";
+}
+if ( $age < 20 ) {
+    echo "未熟です \n";
+}
+
+//elseで書き換え
+$age = 19 ;
+if ( $age >= 20 ) {
+    echo "成人です \n";
+} else {
+    echo "未成年です \n";
+}
+
+//elseifつき
+$age = 19 ;
+if ( $age > 20 ) {
+    echo "成人です \n";
+} else if ( $age === 20 ) {
+    echo "新成人おめでとう \n";
+} else {
+    echo "未成年です \n";
+}

099bb26703be4a4368d3440ef13653b675bf4dee
EC2 Default User <ec2-user@ip-172-31-77-95.ec2.internal>
2022-03-14 13:12:58 +0000

    greater_lesser.phpを書いた

---
+++ greater_lesser.php
@@ -0,0 +1,15 @@
+<?php
+
+$age = 20;
+
+//越える / 未満
+var_dump( $age > 20 );
+var_dump( $age < 20 );
+var_dump( $age > 21 );
+var_dump( $age < 21 );
+var_dump( $age > 19 );
+var_dump( $age < 19 );
+
+//以上 / 以下
+var_dump( $age >= 20 );
+var_dump( $age <= 20 );

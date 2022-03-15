3e32ac4560a0902b109c07fae5ccf3568487c8f2
EC2 Default User <ec2-user@ip-172-31-77-95.ec2.internal>
2022-03-15 14:45:39 +0000

    explode_implode.php

---
+++ explode_implode.php
@@ -0,0 +1,19 @@
+<?php
+
+//元の文字列:「；」で区切られていると想定
+$base_string = "Stollen;Panettone;Pandoro;Kouglof;Lussekatter";
+echo "元の文字列: \n";
+var_dump( $base_string );
+
+//explode()で、；のところで分解
+$exploded_array = explode( ";" , $base_string );
+echo "\n explodeした配列 \n";
+var_dump( $exploded_array );
+foreach ($exploded_array as $value) {
+    echo "{$value} \n";
+}
+
+//implode()で、,(カンマ)で繋げて再構成
+$imploded_string = implode( "," , $exploded_array );
+echo "\n implodeで繋げた文字列 \n";
+var_dump( $imploded_string );
